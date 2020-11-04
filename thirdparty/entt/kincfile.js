c864ead381602902c5b0e1189dd9beb8df4bc12f
Paolo <paolo.oliverio.dev@gmail.com>
2020-11-04 14:08:08 +0100

    remove microsoft containers specific optimization from unrelated platforms.

--- thirdparty/entt/kincfile.js
+++ thirdparty/entt/kincfile.js
@@ -2,7 +2,10 @@ let project = new Project('Entt');
 project.addIncludeDir('entt/src');
 project.addFile('entt/src/**');
 project.addDefine("KISS_ENTT");
-project.addDefine("_ITERATOR_DEBUG_LEVEL#0");
-project.addDefine("_HAS_ITERATOR_DEBUGGING#0");
-project.addDefine("ENTT_DISABLE_ASSERT");
+if(platform == 'windows' || platform == 'windowsapp'){
+    project.addDefine("_ITERATOR_DEBUG_LEVEL#0");
+    project.addDefine("_HAS_ITERATOR_DEBUGGING#0");
+}
+project.addDefine("ENTT_ASSERT(x)");
+project.cppstd = 17;
 resolve(project);
