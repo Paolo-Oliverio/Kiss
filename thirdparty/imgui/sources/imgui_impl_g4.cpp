6c60786529a646ab236b7bd0368ed59eeeff3501
Paolo <paolo.oliverio.dev@gmail.com>
2020-11-19 16:55:41 +0100

    imgui update to new rendering refactor

--- thirdparty/imgui/sources/imgui_impl_g4.cpp
+++ thirdparty/imgui/sources/imgui_impl_g4.cpp
@@ -6,34 +6,31 @@
 
 //#include <kinc/pch.h>
 #include <Kiss/pch.h>
+
 #include <kinc/graphics4/graphics.h>
-#include <kinc/graphics4/pipeline.h>
-#include <kinc/graphics4/indexbuffer.h>
-#include <kinc/graphics4/shader.h>
-#include <kinc/graphics4/texture.h>
 #include <kinc/graphics4/vertexbuffer.h>
 #include <kinc/graphics4/vertexstructure.h>
+#include <kinc/graphics4/indexbuffer.h>
+#include <kinc/graphics4/pipeline.h>
+#include <kinc/graphics4/texture.h>
 
 #include "imgui.h"
 #include "imgui_impl_g4.h"
 
 #include <Kiss/render/gfx2dDefs.h>
+#include <Kiss/render/bufferManager.h>
 
-// G4 data
-static kinc_g4_texture_t g_Texture;
-static bool g_FontSamplerInitialized = false;
-static kinc_g4_index_buffer_t g_IB;
-static bool g_IndexBufferInitialized = false;
-static kinc_g4_vertex_buffer_t g_VB;
-static bool g_VertexBufferInitialized = false;
-static int  g_VertexBufferSize = 5000, g_IndexBufferSize = 10000;
+namespace {
+    static kinc_g4_texture_t g_Texture;
+    static bool g_FontSamplerInitialized = false;
+}
 
 using namespace kiss;
 using namespace kiss::gfx2d;
 
 static void ImGui_ImplG4_SetupRenderState(ImDrawData* draw_data)
 {
-    kinc_g4_set_pipeline(&Pipe2d.pipe);
+    kinc_g4_set_pipeline(&base::pipeline.pipe);
 }
 
 struct ImKincVert
@@ -48,31 +45,16 @@ struct ImKincVert
 // (this used to be set in io.RenderDrawListsFn and called by ImGui::Render(), but you can now call this directly from your main loop)
 void ImGui_ImplG4_RenderDrawData(ImDrawData* draw_data)
 {
-    // Create and grow vertex/index buffers if needed
-    if (!g_VertexBufferInitialized || g_VertexBufferSize < draw_data->TotalVtxCount)
-    {
-		if (g_VertexBufferInitialized) { kinc_g4_vertex_buffer_destroy(&g_VB); }
-        g_VertexBufferSize = draw_data->TotalVtxCount + 5000;
-		kinc_g4_vertex_buffer_init(&g_VB, g_VertexBufferSize, &Pipe2d.vertexLayout, KINC_G4_USAGE_DYNAMIC, 0);
-		g_VertexBufferInitialized = true;
-    }
-    if (!g_IndexBufferInitialized || g_IndexBufferSize < draw_data->TotalIdxCount)
-    {
-        if (g_IndexBufferInitialized) { kinc_g4_index_buffer_destroy(&g_IB); }
-        g_IndexBufferSize = draw_data->TotalIdxCount + 10000;
-		kinc_g4_index_buffer_init(&g_IB, g_IndexBufferSize, KINC_G4_INDEX_BUFFER_FORMAT_16BIT);
-		g_IndexBufferInitialized = true;
-    }
+    kinc_g4_vertex_buffer_t* g_VB = vbuffer::manager.get();
+    kinc_g4_index_buffer_t* g_IB = ibuffer::manager.get();
 
-    // Upload vertex/index data into a single contiguous GPU buffer
-	ImKincVert* vtx_dst = (ImKincVert*)kinc_g4_vertex_buffer_lock(&g_VB,0, draw_data->TotalVtxCount);
-    auto* idx_dst = kinc_g4_index_buffer_lock(&g_IB);
+	ImKincVert* vtx_dst = (ImKincVert*)kinc_g4_vertex_buffer_lock(g_VB,0, draw_data->TotalVtxCount);
+    auto* idx_dst = kinc_g4_index_buffer_lock(g_IB);
 
     int global_vtx_offset = 0;
     for (int n = 0; n < draw_data->CmdListsCount; n++)
     {
         const ImDrawList* cmd_list = draw_data->CmdLists[n];
-        //memcpy(vtx_dst, cmd_list->VtxBuffer.Data, cmd_list->VtxBuffer.Size * sizeof(ImDrawVert));
         for (int i = 0; i < cmd_list->VtxBuffer.Size; ++i)
         {
             vtx_dst[i].pos = cmd_list->VtxBuffer.Data[i].pos;
@@ -84,25 +66,25 @@ void ImGui_ImplG4_RenderDrawData(ImDrawData* draw_data)
         {
             idx_dst[i] =(u32) cmd_list->IdxBuffer.Data[i] + global_vtx_offset;
         }
+        //TODO this cannot be done due to index buffer 32 16 implementation.
         //memcpy(idx_dst, cmd_list->IdxBuffer.Data, cmd_list->IdxBuffer.Size * sizeof(ImDrawIdx));
         vtx_dst += cmd_list->VtxBuffer.Size;
         idx_dst += cmd_list->IdxBuffer.Size;
         global_vtx_offset += cmd_list->VtxBuffer.Size;
     }
-	kinc_g4_vertex_buffer_unlock(&g_VB, draw_data->TotalVtxCount);
-	kinc_g4_index_buffer_unlock(&g_IB); 
+	kinc_g4_vertex_buffer_unlock(g_VB, draw_data->TotalVtxCount);
+	kinc_g4_index_buffer_unlock(g_IB); 
     
     // Setup desired DX state
     ImGui_ImplG4_SetupRenderState(draw_data);
 
     {// Setup Projection.
-        kinc_g4_set_matrix3(Pipe2d.proj_location, &Pipe2d.guiProjection);
+        kinc_g4_set_matrix3(base::pipeline.proj_location, &xform::gui);
     }
 
     // Render command lists
     // (Because we merged all buffers into a single one, we maintain our own offset into them)
     int global_idx_offset = 0;
-    //int global_vtx_offset = 0;
     ImVec2 clip_off = draw_data->DisplayPos;
     for (int n = 0; n < draw_data->CmdListsCount; n++)
     {
@@ -121,14 +103,13 @@ void ImGui_ImplG4_RenderDrawData(ImDrawData* draw_data)
             }
             else
             {
-               // ImGui_ImplG4_SetupRenderState(draw_data);
                 // Apply scissor/clipping rectangle
 				kinc_g4_scissor((int)(pcmd->ClipRect.x - clip_off.x), (int)(pcmd->ClipRect.y - clip_off.y), (int)(pcmd->ClipRect.z - clip_off.x), (int)(pcmd->ClipRect.w - clip_off.y));
 
                 // Bind texture, Draw
-				kinc_g4_set_texture(Pipe2d.texture_unit, (kinc_g4_texture*)pcmd->TextureId);
-				kinc_g4_set_vertex_buffer(&g_VB);
-				kinc_g4_set_index_buffer(&g_IB);
+				kinc_g4_set_texture(base::pipeline.texture_unit, (kinc_g4_texture*)pcmd->TextureId);
+				kinc_g4_set_vertex_buffer(g_VB);
+				kinc_g4_set_index_buffer(g_IB);
                 kinc_g4_draw_indexed_vertices_from_to(pcmd->IdxOffset + global_idx_offset, pcmd->ElemCount);
 				//kinc_g4_draw_indexed_vertices_from_to_from(pcmd->IdxOffset + global_idx_offset, pcmd->ElemCount, pcmd->VtxOffset + global_vtx_offset);
             }
@@ -177,11 +158,7 @@ bool ImGui_ImplG4_CreateDeviceObjects()
 void ImGui_ImplG4_InvalidateDeviceObjects()
 {
     kinc_g4_texture_destroy(&g_Texture);
-    kinc_g4_vertex_buffer_destroy(&g_VB);
-    kinc_g4_index_buffer_destroy(&g_IB);
     g_FontSamplerInitialized = false;
-    g_IndexBufferInitialized = false;
-    g_VertexBufferInitialized = false;
 }
 
 bool ImGui_ImplG4_Init(int window)
