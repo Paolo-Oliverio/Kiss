696f339fd88224b7ce326e9543d89e270530b03e
Paolo <paolo.oliverio.dev@gmail.com>
2020-11-19 16:53:28 +0100

    minimum delta time

--- engine/src/Kiss/time/delta.cpp
+++ engine/src/Kiss/time/delta.cpp
@@ -9,9 +9,9 @@ namespace kiss{
 		ticks += 1;
 		float dt = (float)(time - lastTime);
 		delta.raw = dt;
-		delta.safe = kinc_clamp(dt, 0.0001f, 0.333f);
+		delta.safe = kinc_min(dt, 0.333f);
 		lastTime = time;
 		sum += dt;
-		return delta.raw;
+		return delta.safe;
 	}
 }
